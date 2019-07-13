997c3b76d1db324063ab1297cefea2d3d5498726
yuri abaev <yuri.abaev@gmail.com>
2019-07-13 19:49:31 +0300

    added btn

--- watering/timet_watering/timet_watering.ino
+++ watering/timet_watering/timet_watering.ino
@@ -8,23 +8,32 @@ unsigned long sinceOn = millis();
 unsigned long sinceOff = millis();
 bool isOn = false;
 const int motorPin=9;
-#define WATERING_TIME 2
+const int switchPin=2;
+#define WATERING_TIME 3
 #define INTEVAL_TIME 259200
 
+
+//motor
 void WaterOn(){
   sinceOn=millis();
-  isOn=true;
-  digitalWrite(motorPin,HIGH);
+  motorOn();
 }
 
 void WaterOff(){
   sinceOff=millis();
+  motorOff();
+}
+void motorOff(){;
   isOn=false;
   digitalWrite(motorPin,LOW);
 }
+void motorOn(){;
+  isOn=true;
+  digitalWrite(motorPin,HIGH);
+}
+//end motor
 
-int timeSince(unsigned long time){
-      
+int timeSince(unsigned long time){ 
   return (millis()-time)/1000;
 }
 
@@ -50,6 +59,17 @@ Serial.print(millis());
 Serial.println(isOn);
   }
 }
+int switchState=0;
+ 
+void handlePressBtn(){
+  switchState = digitalRead(switchPin);
+  Serial.println(switchState);
+  if(switchState==HIGH){
+      motorOn();
+  }else{
+      motorOff();
+  }
+}
 
 void setup() {
     Serial.begin(9600);
@@ -58,5 +78,7 @@ void setup() {
 }
 
 void loop() {
+    handlePressBtn();
     handleWatering();
+//    delay(1000);
 }
